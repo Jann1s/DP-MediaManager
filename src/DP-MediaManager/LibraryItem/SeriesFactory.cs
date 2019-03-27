aa251989d0bc7308eacebbd8310abad3f7eec920
Jann1s <luegering.jannis@googlemail.com>
2019-03-27 18:56:10 +0100

    Project files created

---
+++ src/DP-MediaManager/LibraryItem/SeriesFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_MediaManager.LibraryItem
+{
+    class SeriesFactory : ILibraryFactory
+    {
+        public void CreateLibraryEntry()
+        {
+
+        }
+    }
+}
---
+++ src/DP-MediaManager/View/ManageEntry.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DP_MediaManager.View
+{
+    /// <summary>
+    /// Interaction logic for ManageEntry.xaml
+    /// </summary>
+    public partial class ManageEntry : Window
+    {
+        public ManageEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
