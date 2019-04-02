2cf1f168c304974cf19f7b55eeb109a4fccde5c8
Jann1s <luegering.jannis@googlemail.com>
2019-04-02 18:37:47 +0200

    Movie adding to the database works

--- src/DP-MediaManager/LibraryItem/LibraryFactory.cs
+++ src/DP-MediaManager/LibraryItem/LibraryFactory.cs
@@ -8,14 +8,14 @@ namespace DP_MediaManager.LibraryItem
 {
     public abstract class LibraryFactory
     {
-        public static LibraryFactory GetLibrary(LibraryType libraryType, int id)
+        public static LibraryFactory GetLibrary(LibraryType libraryType, int id, string genre)
         {
             switch (libraryType)
             {
                 case LibraryType.Movie:
-                    return new Movie(id);
+                    return new Movie(id, genre);
                 case LibraryType.Series:
-                    return new Series(id);
+                    return new Series(id, genre);
                 default:
                     return null;
             }
@@ -26,6 +26,8 @@ namespace DP_MediaManager.LibraryItem
         public abstract Entry GetDetails(int season = -1, int episode = -1);
 
         public abstract int GetId();
+
+        public abstract string GetGenre();
     }
 
     public enum LibraryType {
